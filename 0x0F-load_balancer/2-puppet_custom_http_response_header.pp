8f526d1a66e82afce53e3108825f5b1e863ef16d
Xage1 <kennethkiarie555@gmail.com>
2024-01-18 02:52:38 -0800

    kdngkjdn

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,16 +1,34 @@
-# Use Puppet to automate the task of creating a custom HTTP header response
-
+# Create a custom header with puppet
 exec {'update':
-  command => '/usr/bin/apt-get update',
+  provider => shell,
+  command  => 'sudo apt-get -y update',
+  before   => Exec['install Nginx'],
 }
--> package {'nginx':
-  ensure => 'present',
+
+exec {'install Nginx':
+  provider => shell,
+  command  => 'sudo apt-get -y install nginx',
+  before   => Exec['add_header'],
 }
--> file_line { 'http_header':
-  path  => '/etc/nginx/nginx.conf',
-  match => 'http {',
-  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
+
+exec { 'add_header':
+  provider    => shell,
+  environment => ["HOST=${hostname}"],
+  command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
+  before      => Exec['restart Nginx'],
 }
--> exec {'run':
-  command => '/usr/sbin/service nginx restart',
+
+exec { 'restart Nginx':
+  provider => shell,
+  command  => 'sudo service nginx restart',
 }
+
+# OR    (combine all the steps above together)
+# configures nginx on a server
+# exec { 'setup_nginx':
+#   provider    => shell,
+#   environment => ["HOST=${hostname}"],
+#   command     => 'apt-get -y update; apt-get -y install nginx;
+#   sudo sed -i "/server {/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+#   service nginx restart'
+# }
--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,10 +7,15 @@ def number_of_subscribers(subreddit):
     to the subreddit"""
     import requests
 
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
+
+
     sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
                             .format(subreddit),
                             headers={"User-Agent": "My-User-Agent"},
                             allow_redirects=False)
+    print(sub_info.text)
     if sub_info.status_code >= 300:
         return 0
 
